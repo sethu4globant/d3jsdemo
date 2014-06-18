7a104b6d06099b091a76a53647a510d2877613b0
Sethuraman <sethuraman.subramanian@claricetechnologies.com>
2014-06-18 11:33:47 +0530

    template and more info

--- app.js
+++ app.js
@@ -1,33 +1,34 @@
 /*
  * Module dependencies
  */
-var express = require('express')
-  , stylus = require('stylus')
-  , nib = require('nib')
+var express = require('express'),
+    stylus = require('stylus'),
+    nib = require('nib')
 
 
-var app = express()
+    var app = express()
 
-function compile(str, path) {
-  return stylus(str)
-    .set('filename', path)
-    .use(nib());
-}
+    function compile(str, path) {
+        return stylus(str)
+            .set('filename', path)
+            .use(nib());
+    }
+
+
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 app.use(express.logger('dev'))
-app.use(stylus.middleware(
-  { src: __dirname + '/public'
-  , compile: compile
-  }
-))
+app.use(stylus.middleware({
+    src: __dirname + '/public',
+    compile: compile
+}))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function (req, res) {
-  res.render('index',
-  { title : 'Home' }
-  )
+app.get('/', function(req, res) {
+    res.render('index', {
+        title: 'Home'
+    })
 })
 
 app.use('/', function(req, res) {
